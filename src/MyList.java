7534c12cb645a28460b1ef86b0e57f35669b7dde
Trinh Hau <congtuqwe@gmail.com>
2018-09-12 23:26:24 +0700

    My List

---
+++ src/MyList.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+
+public class MyList<E> {
+    private int size = 0;
+    private static final int DEFAULT_CAPACITY = 10;
+    private Object elements[];
+
+    public MyList() {
+        elements = new Object[DEFAULT_CAPACITY];
+    }
+
+    public void ensureCapa() {
+        int newSize = elements.length * 2;
+        elements = Arrays.copyOf(elements, newSize);
+    }
+
+    public void add(E e) {
+        if (size == elements.length) {
+            ensureCapa();
+        }
+        elements[size++] = e;
+    }
+
+    public E get(int i) {
+        if (i < 0 || i >= size) {
+            throw new IndexOutOfBoundsException("Index " + i+ ", Size " + i);
+        }
+        return (E) elements[i];
+    }
+
+    public static void main(String[] args) {
+        MyList<Integer> listInteger = new MyList<Integer>();
+        listInteger.add(1);
+        listInteger.add(2);
+        listInteger.add(3);
+        listInteger.add(4);
+
+        //System.out.println("Element 1: "+ listInteger.get(6));
+
+        listInteger.get(-1);
+        System.out.println("element -1: " + listInteger.get(-1));
+
+    }
+}
